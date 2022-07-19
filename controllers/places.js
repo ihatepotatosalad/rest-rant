e39d69d86d4ffe2e62d4d874286348324e70567c
ihatepotatosalad <101296339+ihatepotatosalad@users.noreply.github.com>
2022-07-19 15:22:25 -0400

    f

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
+const Def = require('./default')
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    res.render('places/index')
 })
+
 
 module.exports = router
---
+++ views/places/index.jsx
@@ -0,0 +1,16 @@
+const React = require('react')
+const Def = require('./default')
+
+function index() {
+    return (
+        <Def>
+            <main>
+                <h1>index page</h1>
+                <p>IIIIndes</p>
+            </main>
+        </Def>
+    )
+}
+
+
+module.exports = index
