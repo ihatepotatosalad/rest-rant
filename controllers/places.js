6f4c0c0f9b06b86618fe58f45d674a52c49ed1d9
ihatepotatosalad <101296339+ihatepotatosalad@users.noreply.github.com>
2022-07-22 22:37:21 -0400

    added delete

--- controllers/places.js
+++ controllers/places.js
@@ -23,6 +23,34 @@ router.post('/', (req, res) => {
     places.push(req.body)
     res.redirect('/places')
 })
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/show', { place: places[id], id })
+
+    }
+})
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
+})
+
+
 
 
 
--- views/default.jsx
+++ views/default.jsx
@@ -4,10 +4,25 @@ function Def(html) {
         <html>
             <head>
                 <title>Title</title>
-                <link rel='stylesheet' href='/style.css' />
+
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link>
+                <link rel='stylesheet' href='/style.css' />
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li>
+                            <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
+
                 {html.children}
             </body>
         </html>
