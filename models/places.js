18e56844f61eaee6677cb079d5f5ba1cb96ffd27
ihatepotatosalad <101296339+ihatepotatosalad@users.noreply.github.com>
2022-07-28 20:00:39 -0400

    added validation aand errors

--- models/places.js
+++ models/places.js
@@ -2,11 +2,18 @@ const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    pic: String,
+    pic: { type: String, default: 'https://loveincorporated.blob.core.windows.net/contentimages/main/waiter-bad-service.jpg' },
     cuisines: { type: String, required: true },
     city: { type: String, default: 'Anytown' },
     state: { type: String, default: 'USA' },
-    founded: Number
+    founded: {
+        type: Number,
+        min: [1673, 'Surley not that old??'],
+        max: [new Date().getFullYear(), 'hey this year is in the future']
+    }
 })
+placeSchema.methods.showEstablished = function () {
+    return `${this.name} has been serving the people of ${this.city},${this.state} since ${this.founded}`
+}
 
 module.exports = mongoose.model('Place', placeSchema)
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,14 +7,15 @@ function show(data) {
             <main>
                 <h1>{data.place.name}</h1>
                 <img src={data.place.pic} alt="" />
+                <h3>located in {data.place.city} , {data.place.state}</h3>
                 <h2>Rating</h2>
                 <h3>not yet</h3>
                 <h2>description</h2>
-                <h3>not yet</h3>
+                <h3>{data.place.showEstablished()}</h3>
                 <h2>Comment </h2>
                 <h3>not yet</h3>
                 <h2>cuisines </h2>
-                <h3>{`${data.place.cuisines}`}</h3>
+                <h3>serving {data.place.cuisines}</h3>
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                     Edit
                 </a>
